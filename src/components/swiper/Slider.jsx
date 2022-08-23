6bc22002e2c600c869ae7d7d3d62d13b700ffe52
unknown <abashia.nikoloza@gmail.com>
2022-08-23 15:46:23 +0400

    e-commerce

--- src/components/swiper/Slider.jsx
+++ src/components/swiper/Slider.jsx
@@ -5,6 +5,7 @@ import image4 from "../../assets/img4.jpg";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper";
+import ReactStars from "react-rating-stars-component";
 
 // Import Swiper stylesD
 import "swiper/css";
@@ -15,7 +16,7 @@ import "./slider.scss";
 
 const Slider = () => {
   return (
-    <>
+    <div className="slider_container">
       <Swiper
         modules={[Autoplay]}
         spaceBetween={0}
@@ -26,19 +27,43 @@ const Slider = () => {
         loop
       >
         <SwiperSlide>
-          <img src={image1} alt="" />
+          <div className="title">
+            <h1>StockX</h1>
+            <img src={image1} alt="" />
+            <div className="stars">
+              <ReactStars size={40} color={"#f4c10f"} />
+            </div>
+          </div>
         </SwiperSlide>
         <SwiperSlide>
-          <img src={image2} alt="" />
+          <div className="title">
+            <h1>StockX</h1>
+            <img src={image2} alt="" />
+            <div className="stars">
+              <ReactStars size={40} color={"#f4c10f"} />
+            </div>
+          </div>
         </SwiperSlide>
         <SwiperSlide>
-          <img src={image3} alt="" />
+          <div className="title">
+            <h1>StockX</h1>
+            <img src={image3} alt="" />
+            <div className="stars">
+              <ReactStars size={40} color={"#f4c10f"} />
+            </div>
+          </div>
         </SwiperSlide>
         <SwiperSlide>
-          <img src={image4} alt="" />
+          <div className="title">
+            <h1>StockX</h1>
+            <img src={image4} alt="" />
+            <div className="stars">
+              <ReactStars size={40} color={"#f4c10f"} />
+            </div>
+          </div>
         </SwiperSlide>
       </Swiper>
-    </>
+    </div>
   );
 };
 
